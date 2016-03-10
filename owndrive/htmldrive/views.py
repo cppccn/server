ff48d77ab6ab915bb4f3ccb021ee53b36edec4a8
Mattia Primavera <sconqua@gmail.com>
2016-03-10 20:49:36 +0100

    Old users and groups deleted

--- owndrive/htmldrive/views.py
+++ owndrive/htmldrive/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from owndrive.htmldrive.serializers import UserSerializer, GroupSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
