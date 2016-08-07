0031642d2d23c08ee584a5a7c99c1a0f2ad5718b
Mattia Primavera <sconqua@gmail.com>
2016-08-07 12:26:32 +0200

    Adding logs to install script

--- install.sh
+++ install.sh
@@ -1,21 +1,29 @@
 # Installing Virtualenv
+echo -e "Installing virtualenv ..."
 pip install virtualenv
 
 # Creating and Activating Virtualenv
+echo -e "Creating new Virtual Environment ..."
 virtualenv env
 source env/bin/activate
 
 # Installing Django pip requirements
+echo -e "Installing PIP requirements ..."
 pip install -r requirements.txt
 
 # Installing Web Client
-git clone git@github.com:cappuccino-app/cappuccino-web.git .
+echo -e
+echo -e "Cloning Cappuccino-Web ..."
+git clone git@github.com:cappuccino-app/cappuccino-web.git
 
 # Creating Static Files Directory for django
-mkdir -p cappuccino-server/owndrive/static
+echo -e "Creating Static Files Directory ..."
+mkdir -p owndrive/static
 
 # Collecting Static Files for serving Web Client Resources
+echo -e "Collecting Static Files ..."
 python manage.py collectstatic
 
 # Creating MySql database
+echo -e "Creating MySql Database ..."
 mysql -u root -e "create database 'cappuccino'" -p
