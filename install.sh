3e48a74bed323f4d67b8fe2a0e8e2449ea42aaaf
Mattia Primavera <sconqua@gmail.com>
2016-08-07 12:15:00 +0200

    Collecting Statics + Creating database

--- install.sh
+++ install.sh
@@ -11,4 +11,11 @@ pip install -r requirements.txt
 # Installing Web Client
 git clone git@github.com:cappuccino-app/cappuccino-web.git .
 
+# Collecting Static Files for serving Web Client Resources
+python manage.py collectstatic
+
+# Creating Static Files Directory for django
 mkdir -p owndrive/static
+
+# Creating MySql database
+mysql -uroot -e "create database 'cappuccino'"
