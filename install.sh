cd4941ac4fbd4f5c29679c942a786538e9cbe348
Mattia Primavera <sconqua@gmail.com>
2016-08-07 19:56:37 +0200

    Including Client into setup

--- install.sh
+++ install.sh
@@ -16,7 +16,10 @@ source env/bin/activate
 # Installing Web Client
 # TODO: Ugly.. maybe a git submodule could be a good idea
 echo -e "Cloning Cappuccino-Web ..."
-git clone git@github.com:cappuccino-app/cappuccino-web.git
+#git clone git@github.com:cappuccino-app/cappuccino-web.git
+wget https://github.com/cappuccino-app/cappuccino-web/archive/master.zip
+unzip master.zip
+mv cappuccino-web-master cappuccino-web
 
 # Installing Django pip requirements
 echo -e "Installing PIP requirements ..."
