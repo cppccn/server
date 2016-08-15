a4553b5de80557b4735dd106269b05682ca62e2b
Mattia Primavera <sconqua@gmail.com>
2016-08-15 20:40:58 +0200

    #29 Dinamically add modules urls

--- cappuccino/urls.py
+++ cappuccino/urls.py
@@ -6,20 +6,23 @@ admin.autodiscover()
 from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from cappuccino import settings
+from cappuccino import local_settings
 from django.contrib.auth.views import login
 from django.contrib.auth.views import logout
 from django.contrib.auth.decorators import login_required
+import logging
+import os
+
+APPS_DIRECTORY = local_settings.PROJECT_ROOT + 'cappuccino/apps'
+
+MODULES_INSTALLED = [module for module in os.listdir(APPS_DIRECTORY) if '.py' not in module]
+
+MODULES_URLS = [url(r'^', include('cappuccino.apps.' + module_name + '.urls')) for module_name in MODULES_INSTALLED]
 
 urlpatterns = [
-    url(r'^', include('cappuccino.apps.client.urls')),
-    url(r'^', include('cappuccino.apps.command.urls')),
-    url(r'^', include('cappuccino.apps.upload.urls')),
-    url(r'^', include('cappuccino.apps.download.urls')),
-    url(r'^', include('cappuccino.apps.login.urls')),
-
     # Comment the following line to disable the admin
     url(r'^admin/', include(admin.site.urls)),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + MODULES_URLS
