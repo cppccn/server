35f642a54ec086276f6792843c6cfaa155ac87f6
Mattia Primavera <sconqua@gmail.com>
2016-08-22 04:26:53 +0200

    LsCommand no args test

--- cappuccino/apps/command/tests.py
+++ cappuccino/apps/command/tests.py
@@ -1,17 +1,43 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+import logging
+from django.test import Client
+from django.contrib.auth.models import User
+from cappuccino import local_settings
+import os
 
+class LsCommandTestCase(TestCase):
+	def setUp(self):
+		self.TEST_DIR = local_settings.SHARED_PATH + '/lscommand-test-dir'
+		self.FILE_PREFIX = 'lscommand-test-'
 
-class SimpleTest(TestCase):
+		# Creating test dir
+		if not os.path.exists(self.TEST_DIR):
+		    os.makedirs(self.TEST_DIR)
 
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+		# Creating 5 files inside named 1.txt to 5.txt
+		for i in range(0,5):
+			open(local_settings.SHARED_PATH + '/' + self.FILE_PREFIX + str(i) + '.txt', 'w')
+
+	def test_ls_no_args(self):
+		# Making ls request and checking response code
+		c = Client()
+		response = c.get('/command/', {'command': 'ls', 'currentDir': '/'})
+		self.assertTrue(response.status_code, 200)
+		
+		# Test all created files are there
+		files = response.json()['data']
+		filenames = [f['name'] for f in files]
+		print filenames
+
+		# Check setUp files info retrieved
+		dirname = self.FILE_PREFIX +'dir'
+		self.assertTrue(dirname in filenames, True)
+
+		for i in range(0, 5):
+			filename = self.FILE_PREFIX + str(i) + '.txt'
+			self.assertTrue(True if filename in filenames else False, True)
+
+		# Clean up
+		for i in range(0, 5):
+			os.remove(local_settings.SHARED_PATH + '/' + self.FILE_PREFIX + str(i) + '.txt')
+		os.rmdir(local_settings.SHARED_PATH + '/' + dirname)
