bc40061850428fed76d305a8a0160691c8893f16
Mattia PRIMAVERA <mprimavera@aedmap.com>
2017-06-27 01:24:52 +0200

    Basic POST / GET Handling

--- src/main/java/com/xtech/App.java
+++ src/main/java/com/xtech/App.java
@@ -2,9 +2,13 @@ package com.xtech;
 
 import fi.iki.elonen.NanoHTTPD;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class App extends NanoHTTPD {
+    public static final String POST_METHOD = "POST";
+    public static final String GET_METHOD = "GET";
 
     public App() throws IOException {
         super(8080);
@@ -22,13 +26,35 @@ public class App extends NanoHTTPD {
 
     @Override
     public Response serve(IHTTPSession session) {
-        String msg = "<html><body><h1>Hello server</h1>\n";
-        Map<String, String> parms = session.getParms();
-        if (parms.get("username") == null) {
-            msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
-        } else {
-            msg += "<p>Hello, " + parms.get("username") + "!</p>";
+        log("METHOD", session.getMethod().toString());
+        String method = session.getMethod().toString();
+        if(method.equals(POST_METHOD)) {
+            try {
+                session.parseBody(new HashMap<String, String>());
+                System.out.println( session.getMethod() + " " + session.getParameters().toString() );
+                return newFixedLengthResponse("Some response.");
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            } catch (ResponseException e) {
+                e.printStackTrace();
+                return null;
+            }
+        } else { // GET_METHOD case
+                log("PARAMS", session.getQueryParameterString());
+                log("Getting parameters", session.getParameters().toString());
+                String msg = "<html><body><h1>Hello server</h1>\n";
+                Map<String, List<String>> parms = session.getParameters();
+                if (parms.get("username") == null) {
+                    msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
+                } else {
+                    msg += "<p>Hello, " + parms.get("username").get(0) + "!</p>";
+                }
+                return newFixedLengthResponse(msg + "</body></html>\n");
         }
-        return newFixedLengthResponse(msg + "</body></html>\n");
+    }
+
+    public void log(String tag, String message) {
+        System.out.println(tag + ": " + message);
     }
 }
