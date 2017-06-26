4186d00897bda1cdd4a269d68bad8d69660a88da
Mattia PRIMAVERA <mprimavera@aedmap.com>
2017-06-27 01:47:03 +0200

    Assets dir added + correctly serving html/css

--- src/main/java/com/xtech/App.java
+++ src/main/java/com/xtech/App.java
@@ -1,14 +1,19 @@
 package com.xtech;
 
 import fi.iki.elonen.NanoHTTPD;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class App extends NanoHTTPD {
     public static final String POST_METHOD = "POST";
     public static final String GET_METHOD = "GET";
+    public static final String BASE_RES_DIR = "./src/assets/client";
+    public static final String ROOT_PAGE = "/";
+    public static final String APP_FILE = "/sample.html";
 
     public App() throws IOException {
         super(8080);
@@ -28,6 +33,8 @@ public class App extends NanoHTTPD {
     public Response serve(IHTTPSession session) {
         log("METHOD", session.getMethod().toString());
         String method = session.getMethod().toString();
+        String uri = session.getUri();
+        log("URI: ", session.getUri());
         if(method.equals(POST_METHOD)) {
             try {
                 session.parseBody(new HashMap<String, String>());
@@ -41,20 +48,56 @@ public class App extends NanoHTTPD {
                 return null;
             }
         } else { // GET_METHOD case
-                log("PARAMS", session.getQueryParameterString());
-                log("Getting parameters", session.getParameters().toString());
-                String msg = "<html><body><h1>Hello server</h1>\n";
-                Map<String, List<String>> parms = session.getParameters();
-                if (parms.get("username") == null) {
-                    msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
-                } else {
-                    msg += "<p>Hello, " + parms.get("username").get(0) + "!</p>";
-                }
-                return newFixedLengthResponse(msg + "</body></html>\n");
+            File file;
+            if(uri.equals(ROOT_PAGE)) {
+                file = new File(BASE_RES_DIR + APP_FILE);
+            } else {
+                file = new File(BASE_RES_DIR + uri); //path exists and its correct
+            }
+            try {
+                log("file path is : ", file.getCanonicalPath());
+                return sendFile(file.getCanonicalPath());
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+            }
+
+/*            log("PARAMS", session.getQueryParameterString());
+            log("Getting parameters", session.getParameters().toString());
+            String msg = "<html><body><h1>Hello server</h1>\n";
+            Map<String, List<String>> parms = session.getParameters();
+            if (parms.get("username") == null) {
+                msg += "<form action='?' method='get'>\n  <p>Your name: <input type='text' name='username'></p>\n" + "</form>\n";
+            } else {
+                msg += "<p>Hello, " + parms.get("username").get(0) + "!</p>";
+            }
+            return newFixedLengthResponse(msg + "</body></html>\n");*/
         }
     }
 
     public void log(String tag, String message) {
         System.out.println(tag + ": " + message);
+    }
+
+    public NanoHTTPD.Response sendFile(String filePath) {
+        FileInputStream fis = null;
+        File file = null;
+        long size = -1;
+        try {
+            file = new File(filePath); //path exists and its correct
+            log("Canonical path", file.getCanonicalPath());
+            size = file.length();
+            fis = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException ioe ) {
+            ioe.printStackTrace();
+        }
+
+        int lastIndex = filePath.lastIndexOf('.');
+        String fileType = filePath.substring(lastIndex + 1, filePath.length());
+
+        return newFixedLengthResponse(Response.Status.OK, "text/" + fileType, fis, file.length());
     }
 }
