5e7e1c21a280cfe2ae4695cf4c7df7137fe7e52f
Yvan Sraka <yvan.sraka@gmail.com>
2017-10-04 19:53:29 +0200

    Use 2 spaces indentation instead of 4

--- src/app.ts
+++ src/app.ts
@@ -13,41 +13,42 @@ const getDirectoryContent = source =>
 // Creates and configures an ExpressJS web server
 class App {
 
-    // ref to Express instance
-    public express: express.Application;
+  // ref to Express instance
+  public express: express.Application;
 
-    //Run configuration methods on the Express instance.
-    constructor() {
-        this.express = express();
-        this.middleware();
-        this.routes();
-    }
+  //Run configuration methods on the Express instance.
+  constructor() {
+    this.express = express();
+    this.middleware();
+    this.routes();
+  }
 
-    // Configure Express middleware.
-    private middleware(): void {
-        this.express.use(logger('dev'));
-        this.express.use(bodyParser.json());
-        this.express.use(bodyParser.urlencoded({ extended: false }));
-        this.express.use(function(req, res, next) {
-          res.header("Access-Control-Allow-Origin", "*");
-          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-          next();
-        });
-    }
+  // Configure Express middleware.
+  private middleware(): void {
+    this.express.use(logger('dev'));
+    this.express.use(bodyParser.json());
+    this.express.use(bodyParser.urlencoded({ extended: false }));
+    this.express.use((req, res, next) => {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers",
+                 "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
+  }
 
-    // Configure API endpoints.
-    private routes(): void {
-        /* This is just to get up and running, and to make sure what we've got is
-         * working so far. This function will change when we start to add more
-         * API endpoints */
-        let router = express.Router();
-        // placeholder route handler
-        router.get('/', (req, res, next) => {
-            res.json(getDirectoryContent("/home/yvan")
-                     .map(path => ({ path, isDirectory: isDirectory(path) })));
-        });
-        this.express.use('/', router);
-    }
+  // Configure API endpoints.
+  private routes(): void {
+    /* This is just to get up and running, and to make sure what we've got is
+     * working so far. This function will change when we start to add more
+     * API endpoints */
+    let router = express.Router();
+    // placeholder route handler
+    router.get('/', (req, res, next) => {
+      res.json(getDirectoryContent("/home/yvan")
+               .map(path => ({ path, isDirectory: isDirectory(path) })));
+    });
+    this.express.use('/', router);
+  }
 
 }
 
