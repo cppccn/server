8b52e03a72c062d886aebff40f3a889d8665b680
Yvan Sraka <yvan.sraka@gmail.com>
2017-10-08 13:35:53 +0200

    Update gulpfile.js with TypeDoc config

--- gulpfile.js
+++ gulpfile.js
@@ -6,18 +6,31 @@ const JSON_FILES = ['src/*.json', 'src/**/*.json'];
 const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('scripts', () => {
-    const tsResult = tsProject.src()
-        .pipe(tsProject());
+  const tsResult = tsProject.src()
+    .pipe(tsProject());
 return tsResult.js.pipe(gulp.dest('dist'));
 });
 
 gulp.task('watch', ['scripts'], () => {
-    gulp.watch('src/**/*.ts', ['scripts']);
+  gulp.watch('src/**/*.ts', ['scripts']);
 });
 
 gulp.task('assets', function() {
-    return gulp.src(JSON_FILES)
-        .pipe(gulp.dest('dist'));
+  return gulp.src(JSON_FILES)
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['watch', 'assets']);
+
+var typedoc = require("gulp-typedoc");
+gulp.task("typedoc", function() {
+  return gulp
+    .src(["src/**/*.ts"])
+    .pipe(typedoc({
+      module: "commonjs",
+      target: "es5",
+      out: "docs/",
+      name: "CappuccinoServer"
+    }))
+  ;
+});
